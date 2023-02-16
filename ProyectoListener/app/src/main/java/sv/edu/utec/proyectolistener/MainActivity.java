1b28c07e779b67b3339b726d972cb148f0f39fb6
CristianAlvarez0 <2503112019@mail.utec.edu.sv>
2023-02-15 20:13:12 -0600

    Listener

---
+++ ProyectoListener/app/src/main/java/sv/edu/utec/proyectolistener/MainActivity.java
@@ -0,0 +1,37 @@
+package sv.edu.utec.proyectolistener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+Button btnPrincipal ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnPrincipal=findViewById(R.id.btnListener);
+
+        btnPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intento=new Intent(getApplicationContext(), Principal.class);
+                startActivity(intento);
+            }
+        });
+    }
+
+    public void Pantalla(View v){
+        Intent intento=new Intent(this,Pantalla3.class);
+        startActivity(intento);
+    }
+    public void Salir(View v){
+        finish();
+    }
+
+}
