3c3a9014a780d4eccfb5c42fa983d6d5faced5ef
CristianAlvarez0 <2503112019@mail.utec.edu.sv>
2023-02-27 19:55:28 -0600

    AppFood

---
+++ AppFood/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout btnInicial;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnInicial=findViewById(R.id.btnInicio);
+
+        btnInicial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Principal.class));
+            }
+        });
+
+    }
+}
