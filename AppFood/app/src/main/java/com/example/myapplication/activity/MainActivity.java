2d9412e31d4a9000ce8a59f66a0cff97ea4b05e7
CristianAlvarez0 <2503112019@mail.utec.edu.sv>
2023-03-01 21:03:23 -0600

    FoodApp

--- AppFood/app/src/main/java/com/example/myapplication/activity/MainActivity.java
+++ AppFood/app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -6,22 +6,28 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout btnInicial;
+    EditText etUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnInicial=findViewById(R.id.btnInicio);
+        etUser = findViewById(R.id.editTextTextPersonName);
+
 
         btnInicial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Principal.class));
+                Intent i = new Intent(getApplicationContext(), Principal.class);
+                i.putExtra("usuario",etUser.getText().toString());
+                startActivity(i);
             }
         });
 
--- AppFood/app/src/main/java/com/example/myapplication/activity/Principal.java
+++ AppFood/app/src/main/java/com/example/myapplication/activity/Principal.java
@@ -3,14 +3,20 @@ package com.example.myapplication.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
 public class Principal extends AppCompatActivity {
-
+TextView etMEN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        etMEN = findViewById(R.id.tvUusario);
+        Bundle bundle=getIntent().getExtras();
+        String info = "Hola, "+bundle.getString("usuario");
+        etMEN.setText(info);
     }
 }
