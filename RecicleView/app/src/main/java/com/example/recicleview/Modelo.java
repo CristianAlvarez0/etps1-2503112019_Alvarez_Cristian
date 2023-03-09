f656142abbb6fe1eabb6b417beee58ef64602862
CristianAlvarez0 <2503112019@mail.utec.edu.sv>
2023-03-08 19:39:21 -0600

    pelis

---
+++ RecicleView/app/src/main/java/com/example/recicleview/Modelo.java
@@ -0,0 +1,14 @@
+package com.example.recicleview;
+
+public class Modelo {
+    private String NomPeli;
+    private String GeneroPeli;
+    private String DirectorPeli;
+    private String AñoPeli;
+    private String ImgPeli;
+    private double Clasificacion;
+    private String FechaPeli;
+    private String DuracionPeli;
+
+}
+
