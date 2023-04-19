800a74e980e070cd8adf863e61c18b81a5633ceb
CristianAlvarez0 <2503112019@mail.utec.edu.sv>
2023-04-18 23:47:47 -0600

    EduChat

--- EduChat/app/src/main/java/sv/edu/utec/educhat/PantallaRegistro.java
+++ EduChat/app/src/main/java/sv/edu/utec/educhat/PantallaRegistro.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class PantallaRegistro extends AppCompatActivity {
 
     TextView RegresaLoging;
+    Button btnIngresoLoging;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,19 @@ public class PantallaRegistro extends AppCompatActivity {
         setContentView(R.layout.activity_pantalla_registro);
 
         RegresaLoging = findViewById(R.id.link_textview);
+        btnIngresoLoging = findViewById(R.id.btnIngreso);
+
+        btnIngresoLoging.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PantallaRegistro.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
         RegresaLoging.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
